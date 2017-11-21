17e2be5a984cf4e95a7d88b7f518f4f4abb1e77a
Julian Cardenas <juliancp20@gmail.com>
2017-11-21 13:12:30 -0600

    Add Create Gig

---
+++ GigHub/ViewModel/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+
+namespace GigHub.ViewModel
+{
+    public class GigFormViewModel
+    {
+        public String Venue { get; set; }
+        public String Date { get; set; }
+        public String Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
