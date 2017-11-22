6674805dbd0f281d8e092a975a0d016ac105c61c
Julian Cardenas <juliancp20@gmail.com>
2017-11-22 14:47:04 -0600

    Add Following Model

--- GigHub/Models/IdentityModels.cs
+++ GigHub/Models/IdentityModels.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.ComponentModel.DataAnnotations;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace GigHub.Models
 {
@@ -14,11 +16,22 @@ namespace GigHub.Models
         [StringLength(100)]
         public String Name { get; set; }
 
+        public ICollection<Following> Followers { get; set; }
+        public ICollection<Following> Followees { get; set; }
+
+        public ApplicationUser()
+        {
+            Followers = new Collection<Following>();
+            Followees = new Collection<Following>();
+
+        }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+          
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
+            
             return userIdentity;
         }
     }
