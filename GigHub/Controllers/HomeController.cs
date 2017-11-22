26f9877f6ca8fb7c29449aa9017f05052fcdeca6
Julian Cardenas <juliancp20@gmail.com>
2017-11-22 11:59:55 -0600

    Improve Index

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -19,8 +19,10 @@ namespace GigHub.Controllers
 
         public ActionResult Index()
         {
+            
             var upComingGigs = _context.Gigs
                                             .Include(g => g.Artist)
+                                            .Include(g => g.Genre)
                                             .Where(g => g.DateTime > DateTime.Now);
 
 
