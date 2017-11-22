f7664e7a59340f58e9cbe6be2c4dbbd355a9e95c
Julian Cardenas <juliancp20@gmail.com>
2017-11-22 11:34:55 -0600

    Updatig Packages

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upComingGigs = _context.Gigs
+                                            .Include(g => g.Artist)
+                                            .Where(g => g.DateTime > DateTime.Now);
+
+
+            return View(upComingGigs);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
