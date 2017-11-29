f90a110c009088fa102f6da1f4c9e6339dc16e1e
Julian Cardenas <juliancp20@gmail.com>
2017-11-29 16:24:29 -0600

    adding underscore.js

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace GigHub
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/underscore.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -28,6 +29,8 @@ namespace GigHub
                       "~/Content/site.css",
                       "~/Content/animate.css"
                       ));
+            
+
         }
     }
 }
